630db2cdd7edc4abd0b191c20a55c2bd0a883580
rene_mbp <harrymillermail@googlemail.com>
2021-02-18 23:10:45 +0100

    change host to setgame-server.azurewebsites.net

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :setgame, SetgameWeb.Endpoint,
   check_origin: ["//localhost:5000", "//setgame-server.azurewebsites.net"],
-  url: [host: "example.com", port: 80],
+  url: [host: "setgame-server.azurewebsites.net", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 # Do not print debug messages in production
 config :logger, level: :info
